d66702234bec88e0247ef177079f15d8e29db164
Smulli <78427713+Smulli@users.noreply.github.com>
2021-02-02 16:07:00 +0100

    Script de ComentarioLinea de UD2 con Eclipse.

---
+++ src/ComentarioLinea.java
@@ -0,0 +1,8 @@
+public class ComentarioLinea{
+	
+	public static void main (String args[]){
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DEL PROGRAMA");
+	}
+}
